a333b9b649ed25e09cdf4f48dc3ece0b2302f599
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2018-02-03 17:57:29 +0100

    Improve specification tests
    - Unit Test Provider is SpecRun+NUnit now
    - Add NUnit Test Adapter
    - Formatting

--- TddBook.Tests.Specifications/Calculator.feature.cs
+++ TddBook.Tests.Specifications/Calculator.feature.cs
@@ -85,6 +85,74 @@ this.ScenarioSetup(scenarioInfo);
             TechTalk.SpecFlow.TestRunnerManager.GetTestRunner().OnTestRunEnd();
         }
     }
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("TechTalk.SpecFlow", "2.2.0.0")]
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [NUnit.Framework.TestFixtureAttribute()]
+    [NUnit.Framework.DescriptionAttribute("Calculator")]
+    public partial class CalculatorFeature_NUnit
+    {
+        
+        private TechTalk.SpecFlow.ITestRunner testRunner;
+        
+#line 1 "Calculator.feature"
+#line hidden
+        
+        [NUnit.Framework.OneTimeSetUpAttribute()]
+        public virtual void FeatureSetup()
+        {
+            testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "Calculator", "\tCalculator that adds numbers correctly", ProgrammingLanguage.CSharp, ((string[])(null)));
+            testRunner.OnFeatureStart(featureInfo);
+        }
+        
+        [NUnit.Framework.OneTimeTearDownAttribute()]
+        public virtual void FeatureTearDown()
+        {
+            testRunner.OnFeatureEnd();
+            testRunner = null;
+        }
+        
+        [NUnit.Framework.SetUpAttribute()]
+        public virtual void TestInitialize()
+        {
+        }
+        
+        [NUnit.Framework.TearDownAttribute()]
+        public virtual void ScenarioTearDown()
+        {
+            testRunner.OnScenarioEnd();
+        }
+        
+        public virtual void ScenarioSetup(TechTalk.SpecFlow.ScenarioInfo scenarioInfo)
+        {
+            testRunner.OnScenarioStart(scenarioInfo);
+        }
+        
+        public virtual void ScenarioCleanup()
+        {
+            testRunner.CollectScenarioErrors();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Add two numbers")]
+        public virtual void AddTwoNumbers()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Add two numbers", ((string[])(null)));
+#line 4
+this.ScenarioSetup(scenarioInfo);
+#line 5
+ testRunner.Given("I have entered 50 into the calculator", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 6
+ testRunner.And("I have also entered 70 into the calculator", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 7
+ testRunner.When("I press add", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 8
+ testRunner.Then("the result should be 120", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+    }
 }
 #pragma warning restore
 #endregion
--- TddBook.Tests.Specifications/CalculatorSteps.cs
+++ TddBook.Tests.Specifications/CalculatorSteps.cs
@@ -4,35 +4,34 @@ using TechTalk.SpecFlow;
 
 namespace TddBook.Tests.Specifications
 {
-[Binding]
-public class CalculatorSteps
-{
-    private readonly CalculatorWithRegisteredNumbers _calculator = new CalculatorWithRegisteredNumbers();
-    private int _result;
-
-    [Given(@"I have entered (.*) into the calculator")]
-    public void GivenIHaveEnteredIntoTheCalculator(int number)
+    [Binding]
+    public class CalculatorSteps
     {
-        _calculator.FirstNumber = number;
-    }
+        private readonly CalculatorWithRegisteredNumbers _calculator = new CalculatorWithRegisteredNumbers();
+        private int _result;
 
-    [Given(@"I have also entered (.*) into the calculator")]
-    public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
-    {
-        _calculator.SecondNumber = number;
-    }
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
 
-
-    [When(@"I press add")]
-    public void WhenIPressAdd()
-    {
-        _result = _calculator.Add();
-    }
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
         
-    [Then(@"the result should be (.*)")]
-    public void ThenTheResultShouldBe(int expectedNumber)
-    {
-        Assert.That(_result, Is.EqualTo(expectedNumber));
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _result = _calculator.Add();
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int expectedNumber)
+        {
+            Assert.That(_result, Is.EqualTo(expectedNumber));
+        }
     }
-}
 }
